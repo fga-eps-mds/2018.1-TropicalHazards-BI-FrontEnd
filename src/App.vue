07d8dfc8fa4d17619141ec9938d6583efed6645c
Yoshida-Eduardo <edry97@hotmail.com>
2018-04-04 13:08:25 -0300

    [ADD] added navbar component, imported in app.vue
    Co-authored-by: João Pedro <joaok8@gmail.com>
    Co-authored-by: Andre Filho <andre.filho001@outlook.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/utils/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
@@ -18,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/utils/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="grey">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#/nu">Criar Usuário</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
