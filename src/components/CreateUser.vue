38d2c91232721ee1f06917b5443b25848ee43633
Yoshida-Eduardo <edry97@hotmail.com>
2018-04-04 13:53:17 -0300

    [FIX] methods getData and sendData
    Co-authored-by: João Pedro <joaok8@gmail.com>

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="CreateUser">
-      <input type="text" v-model="input.firstname" placeholder="Username" />
-      <input type="text" v-model="input.lastname" placeholder="Password" />
-      <!-- <input type="text" v-model="input.email" placeholder="Email" /> -->
+      <input type="text" v-model="input.username" placeholder="Username" />
+      <input type="password" v-model="input.password" placeholder="Password" />
+      <input type="text" v-model="input.email" placeholder="Email" />
       <button v-on:click="sendData()">Send</button>
       <button v-on:click="getData()">Get</button>
 
@@ -11,7 +11,7 @@
 
       <textarea>{{ response_get }}</textarea>
       <textarea>{{ response_post }}</textarea>
-      
+
   </div>
 </template>
 
@@ -23,25 +23,25 @@
                 user: "",
                 users:[],
                 input: {
-                    firstname: "",
-                    lastname: "",
-                    // email: ""
+                    username: "",
+                    password: "",
+                    email: ""
                 },
                 response_get: "",
-                response_post:""  
+                response_post:""
             }
         },
-        
+
         methods: {
             sendData() {
-              this.$http.post('https://httpbin.org/post', this.input, { headers: { "content-type": "application/json" } }).then(result => {
+              this.$http.post('http://localhost:8000/users/', this.input, { headers: { "content-type": "application/json" } }).then(result => {
                   this.response_post = result.data;
                 }, error => {
                     console.error(error);
                 });
             },
               getData() {
-              this.$http.get('https://httpbin.org/ip', this.input, { headers: { "content-type": "application/json" } }).then(result => {
+              this.$http.get('http://localhost:8000/users/', { headers: { "content-type": "application/json" } }).then(result => {
                   this.response_get = result.data;
                 }, error => {
                     console.error(error);
