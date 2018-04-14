4a0a2603be580b1d6baee8afdf3bd7ed2a35a6bf
jppgomes <joaok8@gmail.com>
2018-04-14 19:29:55 -0300

    [DELETE] delete Location Map

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,12 @@
 </template>
 
 <script>
-import Navbar from '@/components/Utils/Navbar'
 import Footer from '@/components/Utils/Footer'
-import LocationMap from '@/components/Utils/LocationMap'
 
 export default {
   name: 'App',
   components: {
-    Navbar,
     Footer,
-    LocationMap,
   }
 }
 </script>
--- src/components/Utils/LandingPage.vue
+++ src/components/Utils/LandingPage.vue
@@ -5,8 +5,8 @@
       </div>
 
       <div id="about">
-        <About></About>      
-      </div>   
+        <About></About>
+      </div>
        <router-view />
 
       <div id="cardsContent">
@@ -23,14 +23,12 @@
 import Navbar from '@/components/Utils/Navbar'
 import About from '@/components/Utils/About'
 import CardsContent from '@/components/Utils/CardsContent'
-import LocationMap from '@/components/Utils/LocationMap'
 export default {
   name: 'LandingPage',
   components: {
     Navbar,
     About,
-    CardsContent,
-    LocationMap    
+    CardsContent
   }
 }
 </script>
