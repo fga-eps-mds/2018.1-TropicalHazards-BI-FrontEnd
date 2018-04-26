e36eb73ee3892011cfa6b164c981c503c9c044f5
Andre Filho <andre.filho001@outlook.com>
2018-04-26 16:59:28 -0300

    [TEST] fixed some methods for jest

--- test/unit/specs/Register.spec.js
+++ test/unit/specs/Register.spec.js
@@ -2,21 +2,22 @@ import Vue from 'vue'
 import { shallow } from "@vue/test-utils"
 import Register from '@/components/Authentication/Register.vue'
 
-describe('Register.vue', () => {
+describe('tests of Register.vue', () => {
   it('verifies the presence of form fields', () => {
     const wrapper = shallow(Register)
-    expect(wrapper.contains('input[type="text"]')).to.be.true
-    expect(wrapper.contains('input[type="password"]')).to.be.true
-    expect(wrapper.contains('input[type="email"]')).to.be.true
-    expect(wrapper.contains('a.btn-large')).to.be.true
+
+    expect(wrapper.contains('input[type="text"]')).toBeTruthy()
+    expect(wrapper.contains('input[type="password"]')).toBeTruthy()
+    expect(wrapper.contains('input[type="email"]')).toBeTruthy()
+    expect(wrapper.contains('a.btn-large')).toBeTruthy()
   })
 
   it('should have blank properties by default', () => {
     const Constructor = Vue.extend(Register)
     const comp = new Constructor().$mount()
 
-    expect(comp.user.username).to.equal('')
-    expect(comp.user.password).to.equal('')
-    expect(comp.user.email).to.equal('')
+    expect(comp.user.username).toEqual('')
+    expect(comp.user.password).toEqual('')
+    expect(comp.user.email).toEqual('')
   })
 })
