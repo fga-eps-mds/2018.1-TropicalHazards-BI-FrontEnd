bcfbbf862eed88f8081ce750cb476e457e34471e
Andre Filho <andre.filho001@outlook.com>
2018-04-27 11:31:48 -0300

    [UPDATE] applies lint (updated with devel)

--- test/unit/specs/Login.spec.js
+++ test/unit/specs/Login.spec.js
@@ -1,21 +1,26 @@
-import Vue from 'vue'
-import Login from '@/components/Authentication/Login'
+// import Vue from "vue"
+// import { mount } from "@vue/test-utils"
+// import Login from "@/components/Authentication/Login"
 
-describe('Login', () => {
-  // tests not passing due to modal approach
-  // when new UI is implemented this will pass ez
+describe("Login", () => {
+    it("placeholder test", () => {
+        expect(true).toBeTruthy()
+    })
 
-  // it('Should render the title', () => {
-  //   const Constructor = Vue.extend(Login)
-  //   const vm = new Constructor().$mount()
+    // tests not passing due to modal approach
+    // when new UI is implemented this will pass ez
 
-  //   expect(vm.$el.querySelector('#ancora .container h1').textContent).to.equal('Login')
-  // })
+    // it("Should render the title", () => {
+    //   const Constructor = Vue.extend(Login)
+    //   const vm = new Constructor().$mount()
 
-  // it('Should render the form', () => {
-  //   const Constructor = Vue.extend(Login)
-  //   const vm = new Constructor().$mount()
+    //   expect(vm.$el.querySelector("#ancora .container h1").textContent).to.equal("Login")
+    // })
 
-  //   expect(vm.$el.querySelector('input[type="text"]')).to.equal(true)
-  // })
+    // it("Should render the form", () => {
+    //   const Constructor = Vue.extend(Login)
+    //   const vm = new Constructor().$mount()
+
+    //   expect(vm.$el.querySelector("input[type="text"]")).to.equal(true)
+    // })
 })
--- test/unit/specs/Register.spec.js
+++ test/unit/specs/Register.spec.js
@@ -1,23 +1,31 @@
-import Vue from 'vue'
+import Vue from "vue"
 import { shallow } from "@vue/test-utils"
-import Register from '@/components/Authentication/Register.vue'
+import Register from "@/components/Authentication/Register.vue"
 
-describe('tests of Register.vue', () => {
-  it('verifies the presence of form fields', () => {
+describe("tests of Register.vue", () => {
     const wrapper = shallow(Register)
 
-    expect(wrapper.contains('input[type="text"]')).toBeTruthy()
-    expect(wrapper.contains('input[type="password"]')).toBeTruthy()
-    expect(wrapper.contains('input[type="email"]')).toBeTruthy()
-    expect(wrapper.contains('a.btn-large')).toBeTruthy()
-  })
+    it("should be a Vue instance", () => {
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
 
-  it('should have blank properties by default', () => {
-    const Constructor = Vue.extend(Register)
-    const comp = new Constructor().$mount()
+    it("verifies the presence of form fields", () => {
+        expect(wrapper.contains("input[type='text']")).toBeTruthy()
+        expect(wrapper.contains("input[type='password']")).toBeTruthy()
+        expect(wrapper.contains("input[type='email']")).toBeTruthy()
+        expect(wrapper.contains("a.btn-large")).toBeTruthy()
+    })
 
-    expect(comp.user.username).toEqual('')
-    expect(comp.user.password).toEqual('')
-    expect(comp.user.email).toEqual('')
-  })
+    it("should have blank properties by default", () => {
+        const Constructor = Vue.extend(Register)
+        const comp = new Constructor().$mount()
+
+        expect(comp.user.username).toEqual("")
+        expect(comp.user.password).toEqual("")
+        expect(comp.user.email).toEqual("")
+    })
+
+    it("", () => {
+
+    })
 })
