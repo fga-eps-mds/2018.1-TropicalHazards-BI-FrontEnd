7c7fbb514c2b1bf5baf39977b89560f77382226d
Andre Filho <andre.filho001@outlook.com>
2018-04-27 11:31:48 -0300

    [UPDATE] applies lint (updated with devel)

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,11 @@
 import Vue from "vue"
 import About from "@/components/Utils/About"
 
-describe('About.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(About)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.container h2').textContent)
-      .toEqual('Sobre')
-  })
+describe("About.vue", () => {
+    it("should render correct contents", () => {
+        const Constructor = Vue.extend(About)
+        const vm = new Constructor().$mount()
+
+        expect(vm.$el.querySelector(".container h2").textContent).toEqual("Sobre")
+    })
 })
