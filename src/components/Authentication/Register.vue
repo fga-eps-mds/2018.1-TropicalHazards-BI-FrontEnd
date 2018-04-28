aaa5e6e6f7ba4f915484e31fc562b0e83e2d7d0e
Andre Filho <andre.filho001@outlook.com>
2018-04-28 01:31:55 -0300

    [TEST] add validation ok tests for register

--- src/components/Authentication/Register.vue
+++ src/components/Authentication/Register.vue
@@ -10,19 +10,19 @@
             <h1> REGISTRAR </h1>
             <input
               v-model="user.username"
-              @input="$v.user.username.$touch()"
               type="text"
-              placeholder="Username">
+              placeholder="Username"
+              @input="$v.user.username.$touch()">
             <input
               v-model="user.password"
-              @input="$v.user.password.$touch()"
               type="password"
-              placeholder="Password">
+              placeholder="Password"
+              @input="$v.user.password.$touch()">
             <input
               v-model="user.email"
-              @input="$v.user.email.$touch()"
               type="email"
-              placeholder="Email">
+              placeholder="Email"
+              @input="$v.user.email.$touch()">
             <a
               v-on:
               click="Register()"
@@ -33,7 +33,7 @@
           <br>
           <br>
 
-          <p> {{ response }}</p>
+          <p> {{ response }} </p>
 
         </div>
       </div>
@@ -44,10 +44,10 @@
 </template>
 
 <script>
-import { required, minLength, between } from "vuelidate/lib/validators"
+import { required, minLength } from "vuelidate/lib/validators"
 
 export default {
-    data(){
+    data () {
         return {
             user: {
                 username: "",
@@ -61,11 +61,9 @@ export default {
         user: {
             username: {
                 required,
-                between: between(3, 50)
             },
             email: {
                 required,
-                between: between(6, 70)
             },
             password: {
                 required,
@@ -74,7 +72,7 @@ export default {
         }
     },
     methods: {
-        Register(){
+        Register () {
             this.$http.post("users/", this.user, { headers: { "content-type": "application/json" } }).then(result => {
                 this.user = result.data
                 this.response = result.data
