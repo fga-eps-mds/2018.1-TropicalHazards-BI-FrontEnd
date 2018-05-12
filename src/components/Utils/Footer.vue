35906880661422c2509e50f3be4e035ac992eca0
Yoshida-Eduardo <edry97@hotmail.com>
2018-05-12 01:03:40 -0300

    [UPDATE] updated footer content
    Co-authored-by: Joao Pedro <joaok8@gmail.com>

--- src/components/Utils/Footer.vue
+++ src/components/Utils/Footer.vue
@@ -1,59 +1,28 @@
 <template>
-  <div id="Footer">
-    <footer class="page-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col l6 s12">
-            <h5 class="white-text">Footer Content</h5>
-            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-          </div>
-          <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
-            <ul>
-              <li>
-                <router-link
-                  :to="{name: ' '} "
-                  class="grey-text text-lighten-3">
-                  Link 1
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{name: ' '} "
-                  class="grey-text text-lighten-3">
-                  Link 2
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{name: ' '} "
-                  class="grey-text text-lighten-3">
-                  Link 3
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  :to="{name: ' '} "
-                  class="grey-text text-lighten-3">
-                  Link 4
-                </router-link>
-              </li>
-            </ul>
-          </div>
+ <footer class="page-footer  blue lighten-1 grey-text text-lighten-5">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Sobre Nós</h5>
+          <p class="grey-text text-lighten-4">
+            O OBSERV é um observatório de dados com interpretação de dados, geração de indicadores e gráficos.
+            Tudo isso para apoiar a visualização dos indicadores dos dados da sua pesquisa.
+          </p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+            <li>
+              <a class="grey-text text-lighten-3" href="https://fga-gpp-mds.github.io/2018.1-TropicalHazards-BI/">Documentação</a>
+            </li>
+            <li>
+              <a class="grey-text text-lighten-3" href="https://github.com/fga-gpp-mds/2018.1-TropicalHazards-BI">Repositório GitHub</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="footer-copyright">
-        <div class="container">
-          © 2014 Copyright Text
-          <router-link
-            :to="{name: ' '}"
-            class="grey-text text-lighten-4 right">
-            More Links
-          </router-link>
-        </div>
-      </div>
-    </footer>
-  </div>
+    </div>
+  </footer>
 </template>
 
 <script>
