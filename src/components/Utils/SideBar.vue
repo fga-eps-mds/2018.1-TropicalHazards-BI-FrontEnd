3c097ac50ca2b846c2945af5d307e52cbaa04cdd
Andre Filho <andre.filho001@outlook.com>
2018-06-03 09:52:08 -0300

    [ADD] sidebar component

--- src/components/Utils/SideBar.vue
+++ src/components/Utils/SideBar.vue
@@ -1,5 +1,92 @@
 <template>
-  <div
+  <div class="wrapper">
+    <nav id="sidebar">
+      <div class="sidebar-header">
+        <span class="fa fa-user"/>
+        {{ username }}
+        <span class="fa fa-bolt"/>
+        <form action="">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Busca">
+            <div class="input-group-append">
+              <button class="btn">
+                <span class="fa fa-search"/>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <ul class="list-unstyled sidebar-links list-group text-capitalize">
+        <li>
+          <router-link
+            :to="{ name: '' }"
+            class="list-group-item">
+            Projetos <span class="fa fa-plus"/>
+          </router-link>
+          <ul class="list-group">
+            <li>
+              <router-link
+                :to="{ name: '' }"
+                class="list-group-item">
+                ver projetos
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                :to="{ name: '' }"
+                class="list-group-item">
+                meus projetos
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                :to="{ name: '' }"
+                class="list-group-item">
+                criar projeto
+              </router-link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <router-link
+            :to="{ name: '' }"
+            class="list-group-item">
+            dashboards <span class="fa fa-plus"/>
+          </router-link>
+          <ul class="list-group">
+            <li>
+              <router-link
+                :to="{ name: '' }"
+                class="list-group-item">
+                ver dashboards
+              </router-link>
+            </li>
+            <li>
+              <router-link
+                :to="{ name: '' }"
+                class="list-group-item">
+                minhas dashboards
+              </router-link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="http://github.com/fga-gpp-mds/2018.1-TropicalHazards-BI">
+            <span class="fa fa-github"/> github
+          </a>
+        </li>
+        <li>
+          <a href="http://fga-gpp-mds.github.io/2018.1-TropicalHazards-BI">
+            <span class="fa fa-repository"/> docs
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <!-- <div
     id="sidebar"
     class="hide-on-small-only col m1 center-align grey lighten-4">
     <ul class="">
@@ -92,7 +179,7 @@
         </li>
       </router-link>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 
@@ -139,9 +226,18 @@ export default {
 }
 </script>
 
-<style>
-::placeholder {
-  /* Most modern browsers support this now. */
-  color: #ffffff;
-}
+<style lang="scss" scoped>
+  @import '@/components/styles/base.scss';
+
+  .wrapper {
+    display: flex;
+  }
+
+  #sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    height: 10vw;
+    background-color: $alt-background-color;
+    color: $alt-font-color;
+  }
 </style>
