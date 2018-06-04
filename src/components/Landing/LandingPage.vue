6d20b7aad22abf824beaa2e6036064a4d75ffe19
Andre Filho <andre.filho001@outlook.com>
2018-06-03 23:37:18 -0300

    [DEL] components at landing page
    [FIX] moved files and fixed routes

---
+++ src/components/Landing/LandingPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <navbar/>
+    <h1>the peacock lost it's pea...</h1>
+    <p>working on it</p>
+    <LandingFooter/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+
+import LandingNavbar from "@/components/Landing/Navbar"
+import Footer from "@/components/Utils/Footer"
+
+export default {
+    name: "LandingPage",
+    components: {
+        LandingNavbar,
+        Footer
+    },
+
+    computed: {
+        ...mapGetters({
+            currentUser: "currentUser"
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
