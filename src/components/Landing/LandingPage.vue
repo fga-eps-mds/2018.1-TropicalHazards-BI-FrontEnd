e9afc99989d7df3a6e744f6bbddb4377002c2e99
Andre Filho <andre.filho001@outlook.com>
2018-06-04 22:04:42 -0300

    [ADD] header component (wip)
    [FIX] responsiveness for footer and navbar
    [FIX] base.scss rule that was causing problems
    
    started to make front-end great again
    
    Co-authored-by: yoshida-kun <edry97@hotmail.com>

--- src/components/Landing/LandingPage.vue
+++ src/components/Landing/LandingPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <Navbar/>
+    <Header/>
     <h1>the peacock lost it's pea...</h1>
     <p>working on it</p>
     <Footer/>
@@ -11,12 +12,14 @@
 import { mapGetters } from "vuex"
 
 import Navbar from "@/components/Landing/Navbar"
+import Header from "@/components/Landing/Header"
 import Footer from "@/components/Utils/Footer"
 
 export default {
     name: "LandingPage",
     components: {
         Navbar,
+        Header,
         Footer
     },
 
--- src/components/Landing/Navbar.vue
+++ src/components/Landing/Navbar.vue
@@ -1,39 +1,41 @@
 <template>
-  <nav id="navbar">
-    <div class="navbar navbar-expand-md navbar-dark dark-bg">
-      <router-link
-        :to="{name: 'LandingPage'}"
-        class="navbar-brand mr-auto">
-        Observ
-      </router-link>
-      <button
-        type="button"
-        name="toggler"
-        class="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#collapseable">
-        <span class="fa fa-bars"/>
-      </button>
-      <div
-        id="collapseable"
-        class="collapse navbar-collapse dark-bg">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <router-link
-              :to="{name: 'auth'}"
-              class="nav-link">
-              Dashboards
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link
-              :to="{name: 'auth'}"
-              class="nav-link">
-              Entrar
-            </router-link>
-          </li>
-        </ul>
-      </div>
+  <nav
+    id="navbar"
+    class="navbar navbar-expand-md navbar-dark dark-bg">
+    <router-link
+      :to="{name: 'LandingPage'}"
+      class="navbar-brand">
+      Observ
+    </router-link>
+    <button
+      type="button"
+      name="toggler"
+      class="navbar-toggler"
+      aria-controls="collapseable"
+      aria-expanded="false"
+      data-toggle="collapse"
+      data-target="#collapseable">
+      <span class="fa fa-bars"/>
+    </button>
+    <div
+      id="collapseable"
+      class="collapse navbar-collapse dark-bg">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link
+            :to="{name: 'Auth'}"
+            class="nav-link">
+            Dashboards
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link
+            :to="{name: 'Auth'}"
+            class="nav-link">
+            Entrar
+          </router-link>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -69,23 +71,31 @@ export default {
 <style lang="scss" scoped>
   @import '../styles/base.scss';
   .navbar {
-    height: 8vh;
-  .navbar-brand {
+    width: 100%;
+    .navbar-brand {
+        height: 100%;
+      }
+    #collapseable {
       height: 100%;
-    }
-    #collapseable .collapse a {
-      @media (min-width: 764px) {
+      @media (max-width: 764px) {
         width: 100vw;
+        font-size: .9em;
+        a {
+          margin-top: .4em;
+          margin-bottom: .4em;
+          padding-top: 1.1em;
+          padding-bottom: 1.1em;
+        }
       }
     }
   }
   a {
     padding: 1em .3em;
-    margin-left: .1em;
-    margin-right: .1em;
+    margin-left: .2em;
+    margin-right: .2em;
     &.nav-link {
       font-size: .8em;
-      transition: all .03s linear;
+      transition: all .5s linear;
       color: $alt-text-color;
       border-bottom: 1px solid transparent;
       &:hover {
