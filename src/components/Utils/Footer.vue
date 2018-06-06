6c4716d7fc7ca7fabdb4ce0deb7084f906088b85
jppgomes <joaok8@gmail.com>
2018-06-06 13:02:18 -0300

    [UPDATE] update footer

--- src/components/Utils/Footer.vue
+++ src/components/Utils/Footer.vue
@@ -1,36 +1,61 @@
 <template>
-  <footer class="container-fluid text-center dark-bg">
-    <div class="row">
-      <div class="col-12">
-        <h5>Observ</h5>
-        <p class="text-center">
-          {{ footerText }}
-        </p>
-      </div>
-    </div>
-    <div class="row">
-      <a
-        href="https://github.com/fga-gpp-mds/2018.1-TropicalHazards-BI"
-        class="col text-center">
-        Github
-      </a>
-      <router-link
-        :to="{name: 'LandingPage'}"
-        class="col text-center">
-        Inicio
-      </router-link>
-      <a
-        href="https://fga-gpp-mds.github.io/2018.1-TropicalHazards-BI"
-        class="col text-center">
-        Docs
-      </a>
+  <!-- Footer -->
+  <footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-4 mx-auto">
+          <!-- Content -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Observ</h5>
+          <p class="white">Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none">
+        <!-- Grid column -->
+        <div class="col-md-4 mx-auto">
+          <!-- Links -->
+          <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
 
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!">Documentação</a>
+            </li>
+            <li>
+              <a href="#!">GitHub</a>
+            </li>
+            <li>
+              <a href="#!">Inicio</a>
+            </li>
+            <li>
+              <a href="#!">Dashboard</a>
+            </li>
+          </ul>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
     </div>
-    <p
-      id="license"
-      class="text-center">
-      Protegido sob a licença MIT
-    </p>
+    <!-- Footer Links -->
+    <hr>
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-3">
+      <li class="list-inline-item">
+        <h5 class="mb-1">Registre-se de graça</h5>
+      </li>
+      <li class="list-inline-item">
+        <a
+          href="#!"
+          class="btn btn-primary btn-blue">Sign up!</a>
+      </li>
+    </ul>
+    <!-- Call to action -->
+    <hr>
+    <!-- Copyright -->
+    <div class="white footer-copyright text-left py-3">Linceça MIT © 2018 Copyright
+    </div>
+    <!-- Copyright -->
   </footer>
 </template>
 
@@ -66,5 +91,14 @@ export default {
       font-size: .7em;
       margin-top: 4em;
     }
+.list-unstyled{
+  color: white;
+}
+.font-weight-bold{
+  color: white;
+}
+.white{
+  color:white;
+}
   }
 </style>
