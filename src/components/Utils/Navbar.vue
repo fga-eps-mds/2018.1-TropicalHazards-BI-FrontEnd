ab9aa12cd7ed60df8a91b945f5c1be7dac647905
jppgomes <joaok8@gmail.com>
2018-06-05 22:44:31 -0300

    [ADD] add component navbar

---
+++ src/components/Utils/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark ">
+    <div class="text-center navbar-brand ">
+      <router-link
+        :to="{ name: 'LandingPage' }">
+        Observ
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/base.scss";
+
+nav {
+  padding-top: 0;
+  padding-bottom: 0;
+  .navbar-brand {
+    width: 150px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+}
+
+</style>
