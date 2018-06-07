aad6c79c9ba1618bc877c91601434999e8f834d0
Andre Filho <andre.filho001@outlook.com>
2018-06-07 11:43:30 -0300

    [ADD] references bar in landing page
    Co-authored-by: Joao <joaok8@gmail.com>
    Co-authored-by: Max <maxhb.df@gmail.com>
    Co-authored-by: yoshida-kun <edry97@hotmail.com>

---
+++ src/components/Landing/RefBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container-fluid text-center">
+    <div class="bg-img">
+      <div class="row">
+        <div class="col-md-4 cell h3">
+        <span style="bolder">
+          {{ nmbrDashes }}
+        </span>
+        Dashboards
+        </div>
+        <div class="col-md-4 cell h3">
+          <span style="bolder">
+            {{ nmbrMembers }}
+          </span>
+          Usuários
+        </div>
+        <div class="col-md-4 cell h3">
+          <span style="bolder">
+            {{ nmbrProjects }}
+          </span>
+          Projetos
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            nmbrDashes: 0,
+            nmbrMembers: 0,
+            nmbrProjects: 0
+        }
+    },
+
+    methods: {
+        getNmbrOfDashes () {
+
+        },
+
+        getNmbrOfMembers () {
+
+        },
+
+        getNmbrOfProjects () {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/base.scss';
+
+  .container-fluid {
+    background: url('../../assets/data-analytics.png') no-repeat center center;
+    background-size: cover;
+    padding: 0;
+
+    .bg-img {
+      background-color: rgba(0, 0, 0, 0.6);
+    }
+  }
+
+  .cell {
+    padding: 3em 1em;
+    background-color: transparent;
+    margin-bottom: 0;
+    color: $alt-text-color;
+
+    span {
+      color: $blue;
+    }
+  }
+</style>
+
