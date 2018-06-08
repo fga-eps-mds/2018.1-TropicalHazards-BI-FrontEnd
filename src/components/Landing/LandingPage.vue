b160198385a990f845e0e139af42af549dc54935
Renatafsouza <renata.rfsouza@gmail.com>
2018-06-07 22:41:52 -0300

    [ADD]Carousel component

--- src/components/Landing/LandingPage.vue
+++ src/components/Landing/LandingPage.vue
@@ -2,8 +2,7 @@
   <div>
     <!-- <Navbar/> -->
     <Header/>
-    <h1>the peacock lost it's pea...</h1>
-    <p>working on it</p>
+    <Carousel/>
   </div>
 </template>
 
@@ -12,12 +11,14 @@ import { mapGetters } from "vuex"
 
 import Navbar from "@/components/Landing/Navbar"
 import Header from "@/components/Landing/Header"
+import Carousel from "@/components/Landing/Carousel"
 
 export default {
     name: "LandingPage",
     components: {
         Navbar,
-        Header
+        Header,
+        Carousel
     },
 
     computed: {
