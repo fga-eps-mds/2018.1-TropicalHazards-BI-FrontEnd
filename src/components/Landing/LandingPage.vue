4089d821fa43d1e7148421278e0974dee749603c
Andre Filho <andre.filho001@outlook.com>
2018-06-11 19:17:14 -0300

    fix(footer): fixes footer display on pages

--- src/components/Landing/LandingPage.vue
+++ src/components/Landing/LandingPage.vue
@@ -4,6 +4,7 @@
     <Carousel/>
     <RefBar/>
     <About/>
+    <Footer/>
   </div>
 </template>
 
@@ -15,15 +16,16 @@ import Header from "@/components/Landing/Header"
 import Carousel from "@/components/Landing/Carousel"
 import RefBar from "@/components/Landing/RefBar"
 import About from "@/components/Landing/About"
+import Footer from "@/components/Utils/Footer"
 
 export default {
-    name: "LandingPage",
     components: {
         Navbar,
         Header,
         Carousel,
         RefBar,
-        About
+        About,
+        Footer
     },
 
     computed: {
