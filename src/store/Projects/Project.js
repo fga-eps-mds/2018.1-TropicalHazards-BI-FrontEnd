6c35918db663ac0d16c88ca05935aec05ca635be
Yoshida-Eduardo <edry97@hotmail.com>
2018-06-12 20:47:36 -0300

    [ADD] added currentProject state It'll be used to consume metabase data from backend
    Co-authored-by: Andre Bargas <andrebargas@gmail.com>

--- src/store/Projects/Project.js
+++ src/store/Projects/Project.js
@@ -2,9 +2,11 @@
 /* eslint-disable */
 import Vue from 'vue'
 const SET_PROJECTS = 'SET_PROJECTS'
+const UPDATE_CURRENT_PROJECT = 'UPDATE_CURRENT_PROJECT'
 
 const state = {
-    projects: []
+    projects: [],
+    currentProject: null
 }
 
 const getters = {
@@ -13,6 +15,9 @@ const getters = {
     },
     getProjectsLength: state =>{
         return state.projects.length
+    },
+    getCurrentProject: state=>{
+        return state.currentProject
     }
 }
 
@@ -20,6 +25,9 @@ const mutations = {
     [SET_PROJECTS](state, payload){
         // Vue.set(state, projects, payload) - EVENTO PARA GERAR REATIVIDADE MAYBE
         state.projects = payload
+    },
+    [UPDATE_CURRENT_PROJECT](state, payload){
+        state.currentProject = payload
     }
 }
 
@@ -31,6 +39,17 @@ const actions = {
                 resolve()
             },
             error => {
+                reject()
+            })
+        })
+    },
+    updateCurrentProject({commit}, projectId){
+        return new Promise((resolve, reject)=>{
+            Vue.http.get("projects/" + projectId, {headers : {"content-type": "application/json"}}).then(response=>{
+                commit(UPDATE_CURRENT_PROJECT, response.data)
+                resolve()
+            },
+            error =>{
                 reject()
             })
         })
