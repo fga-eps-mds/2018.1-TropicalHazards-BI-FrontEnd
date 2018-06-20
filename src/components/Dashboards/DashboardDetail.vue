90461c3ad4420ba443faf7127d9ad32b2eef5d10
Andre Filho <andre.filho001@outlook.com>
2018-06-20 10:47:36 -0300

    [ADD] wip dashboard detail

--- src/components/Dashboards/DashboardDetail.vue
+++ src/components/Dashboards/DashboardDetail.vue
@@ -1,91 +1,102 @@
 <template>
-  <div class="row grey lighten-4">
-    <sidebar/>
-    <div
-      id="content"
-      class="col m11">
-      <div class="header center-align white">
-        <small>Projeto: Mapa da Dengue no DF</small>
-        <h3>
-          {{ dashboard.name }}
-        </h3>
-      </div>
-      <div class="grey lighten-4">
-        <div class="custom-container">
-
-          <h5>Dados do Observatório</h5>
-
-          <div class="row">
-            <div class="col s12 m4">
-              <div class="card">
-                <div class="card-content">
-                  <span class="card-title">
-                    Cleiton Jr.
-                  </span>
-                  <p>
-                    Criador
-                  </p>
-                </div>
-              </div>
-              <button
-                class = "btn-large red"
-                v-on:
-                @click="deleteDashboard()">Deletar Dashboard
-                <span class="fa fa-trash"/>
-              </button>
-              <!-- <button
-                class = "btn-large blue"
-                @click="$modal.show('ask-question', { dashboard: dashboard.id })">Gerar indicador
-                <span class="fa fa-trash"/>
-              </button>
-              <modal-question/> -->
-            </div>
-            <div class="col s12 m4">
-              <div class="card">
-                <div class="card-content">
-                  <span class="card-title">1.3 Gb</span>
-                  <p>
-                    De dados
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="custom-container">
-          <div class="row">
-            <div class="col s12">
-              <!-- vai ser iframe então lembrar de colocar tag de iframe com a classe de reixar IFRAME RESPONSIVO -->
-              <!-- <iframe
-                src="{{ iframeUrl}}" /> -->
-            </div>
-          </div>
-          <div class="row white darken-1">
-            <div class="col s12">
-              <a
-                href="#"
-                class="btn-large waves-effect waves-light blue lighten-1 white-text">
-                <span class="fa fa-download">Baixar dados usados</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="container-fluid">
+    <div v-if="showError">
+      <header>
+        <h2> Detalhes do Dashboard </h2>
+      </header>
+      <b-jumbotron
+        bg-variant="muted"
+        class="text-muted">
+        <template
+          slot="header">
+          Oooops
+        </template>
+        <template
+          slot="lead">
+          O Dashboard que você está tentando acessar não existe...
+        </template>
+        <p>
+          Não se preocupe, você ainda pode criar um Dashboard e começar sua
+          jornada conosco!
+        </p>
+        <!-- fix link -->
+        <router-link
+          :to="{}"
+          class="btn btn-blue ml-auto">
+          <span class="fa fa-arrow-left"/> Voltar ao projeto
+        </router-link>
+      </b-jumbotron>
     </div>
+    <section v-else>
+      <header>
+        <h2>
+          {{ dashboard.name != '' ? dashboard.name : 'dashboard name' }}
+          <br>
+          <small class="text-muted h4">
+            {{ dashboard.project != '' ? dashboard.project : 'project name' }}
+          </small>
+        </h2>
+        <p class="text-justify">
+          {{ dashboard.description }}
+        </p>
+        <ul class="list-inline">
+          <li
+            v-for="tag in dashboard.project.tags"
+            :key="tag.id"
+            class="list-inline-item">
+            <span class="badge badge-primary btn-blue">
+              {{ tag.name }}
+            </span>
+          </li>
+        </ul>
+      </header>
+      <div class="row">
+        <div
+          v-for="dashboard in dashboards"
+          :key="dashboard.id"
+          class="card col col-md-6">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe
+              :src="dashboard.iframe"
+              class="embed-responsive-item"
+              frameborder="0"/>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">
+              {{ dashboard.name }}
+              <span
+                v-if="dashboard.owner == currentUser"
+                class="badge badge-secondary h6">
+                owner
+              </span>
+            </h5>
+            <p class="card-text">
+              {{ dashboard.description }}
+            </p>
+            <router-link
+              :to="{ name: 'DashboardDetail' }"
+              class="btn btn-blue btn-sm">
+              <span class="fa fa-search"/> Visualizar
+            </router-link>
+          </div>
+        </div>
+
+        <section>
+
+        </section>
+
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 
-import {mapGetters} from "vuex"
-import Sidebar from "@/components/Utils/Sidebar"
+import { mapGetters } from "vuex"
 
 export default {
 
-    components: {
-        "sidebar": Sidebar,
-    },
-    data(){
+    data () {
 
         return {
             dashboard: {
@@ -96,8 +107,10 @@ export default {
             user: {
                 username: "",
                 password: "",
-                email: ""
-            }
+                email: "",
+                id: ""
+            },
+            showError: false
         }
     },
     computed: {
@@ -141,7 +154,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 
 </style>
